8695d381ef7519ce2762eea21af55d905b5ee301
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2024-11-30 20:15:41 +0530

    feat: welcome message on root api

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AuthModule } from './auth/auth.module';
 import { BrandModule } from './brand/brand.module';
 import { PrismaService } from './prisma.service';
 import { LoyaltyPointsModule } from './loyaltyPoints/loyaltyPoints.module'; 
+import { AppController } from './app.controller';
 
 @Module({
   imports: [AuthModule,BrandModule,LoyaltyPointsModule,UserModule],
+   controllers: [AppController], 
   providers: [PrismaService],
 })
 export class AppModule {}
